84ac4d8b7bb5fcacdee8843967cdb467856c2a8c
Joana <joanajanefdez@gmail.com>
2022-07-14 18:55:39 +0200

    pruebas en main class Lavadora

--- src/MainApp.java
+++ src/MainApp.java
@@ -13,7 +13,18 @@ public class MainApp {
 		
 		double precioFinal = electroU9_3.precioFinal();
 		
-		System.out.println(precioFinal);
+		System.out.println("electro3 precio final "+precioFinal);
+		
+		Lavadora lava1 = new Lavadora();
+		Lavadora lava2 = new Lavadora(560,120);
+		
+		Lavadora lava3 = new Lavadora(430,Color.GRIS,Consumo.D,135,50);
+		
+		System.out.println(lava2.getColor());
+		System.out.println(lava2.getConsumoEnergetico());
+		System.out.println(lava2.getCarga());
+		
+		System.out.println("precio final lava3 "+lava3.precioFinal());
 	}
 
 }
