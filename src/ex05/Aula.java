071e3b9a73f07979bbdf86ee35f46a278ac8be23
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-07-15 20:58:08 +0200

    Creamos clase main (Ex5App)

--- src/ex05/Aula.java
+++ src/ex05/Aula.java
@@ -15,32 +15,23 @@ public class Aula {
 		this.estudiantes = estudiantes;
 	}
 	
-	public void puedeDarseClase() {
+	public boolean puedeDarseClase() {
 		if(profesor.isPresente() == false) {
 			System.out.println("No se puede dar clase. El profesor no ha asistido.");
-			return;
+			return false;
 		}
 		if(materia != profesor.getMateria()) {
 			System.out.println("No se puede dar clase. El profesor no imparte esta materia.");
-			return;
+			return false;
 		}
 		if(getNumEstudiantes() <= nEstudiantes/2) {
-			System.out.println("No se puede dar clase. Se necesitan m�s de la mitad de alumnos.");
-			return;
+			System.out.println("No se puede dar clase. Se necesitan mas de la mitad de alumnos.");
+			return false;
 		}
-
-		System.out.println("--- HOMBRES APROBADOS ---");
-		for(Estudiante e : estudiantes) {
-			if (e.getNota() >= 5 && e.getSexo() == Sexo.HOMBRE)
-				System.out.println(e.toString());
-		}
-		
-		System.out.println("--- MUJERES APROBADAS ---");
-		for(Estudiante e : estudiantes) {
-			if (e.getNota() >= 5 && e.getSexo() == Sexo.MUJER)
-				System.out.println(e.toString());
-		}
+	
+		return true;
 	}
+	
 	
 	private int getNumEstudiantes() {
 		int n = 0;
