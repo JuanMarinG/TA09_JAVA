4c537167e76ba73c0e640673f87bc62ceae663e7
Joana <joanajanefdez@gmail.com>
2022-07-15 18:39:13 +0200

    estructura inicial classes -atributos/getters/contructores

--- src/ex06/Cine.java
+++ src/ex06/Cine.java
@@ -2,8 +2,47 @@ package ex06;
 
 public class Cine {
 
+	private Pelicula pelicula;
+	private double precio;
+	
+	
+	//?????? hay otra estructura mas idonea?
+	private int asientos[][] = new int [8][8];
+	
 	public Cine() {
-		// TODO Auto-generated constructor stub
+		this(null,0);
+	}
+	
+	public Cine(Pelicula peli,double precio) {
+		this.pelicula = peli;
+		this.precio = precio;
+	}
+	
+	
+	public Pelicula getPelicula() {
+		return pelicula;
+	}
+
+	public void setPelicula(Pelicula pelicula) {
+		this.pelicula = pelicula;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	
+	//????? array bidi ?
+	public int[][] getAsientos() {
+		return asientos;
+	}
+
+	public void setAsientos(int[][] asientos) {
+		this.asientos = asientos;
 	}
 
 }
--- src/ex06/Espectador.java
+++ src/ex06/Espectador.java
@@ -1,5 +1,43 @@
 package ex06;
 
 public class Espectador {
+	
+	private String nombre;
+	private int edad;
+	private double dinero;
+	
+	public Espectador() {
+		this("",0,0);
+	}
+	
+	public Espectador(String nombre,int edad,double dinero) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dinero = dinero;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public double getDinero() {
+		return dinero;
+	}
+
+	public void setDinero(double dinero) {
+		this.dinero = dinero;
+	}
 
 }
