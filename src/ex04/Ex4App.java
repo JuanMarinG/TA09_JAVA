6a2b185f520aca1e7bac2848d10976c59debfa9f
Arini11 <elarnaumc@gmail.com>
2022-07-15 09:48:01 +0200

    setup ex4

---
+++ src/ex04/Ex4App.java
@@ -0,0 +1,9 @@
+package ex04;
+
+public class Ex4App {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/ex04/Raices.java
@@ -0,0 +1,36 @@
+package ex04;
+
+public class Raices {
+	private double a,b,c;
+
+	public Raices(double a, double b, double c) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	public double getDiscriminate() {
+		
+	}
+	
+	public boolean tieneRaices() {
+		
+	}
+	
+	public boolean tieneRaiz() {
+		
+	}
+	
+	public void calcular() {
+		
+	}
+	
+	public void obtenerRaices() {
+		
+	}
+	
+	public void obtenerRaiz() {
+	}
+
+}
