ad2329f462309643301b4e2939e0e04e13a32942
Joana <joanajanefdez@gmail.com>
2022-07-15 12:46:42 +0200

    subclass Professor - falta implementar metodo faltaAssistencia()

---
+++ src/ex05/Profesor.java
@@ -0,0 +1,25 @@
+package ex05;
+
+public class Profesor extends Persona{
+	
+	private Materia materia;
+	
+	public Profesor() {
+		super();
+		this.materia=""; //al ser enum, ver que se tendria que poner
+	}
+	
+	public Profesor(String nombre,int edad,String genero,Materia materia) {
+		super(nombre,edad,genero);
+		this.materia = materia;
+	}
+
+	@Override
+	public void faltaAssistencia() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
