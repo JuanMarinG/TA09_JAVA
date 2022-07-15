818ff7a72fab24970bd92c1e2a724becbc16b0b2
Joana <joanajanefdez@gmail.com>
2022-07-15 13:07:11 +0200

    eliminar comentarios interfacde Entregable

--- src/ex02/Entregable.java
+++ src/ex02/Entregable.java
@@ -2,37 +2,12 @@ package ex02;
 
 public interface Entregable {
 	
-	public void entregar(); // public void entregar() {	this.delivered = true;}
+	public void entregar();
 	
-	public void devolver(); // public void devolver() {	this.delivered = false;}
+	public void devolver(); 
 	
-	public boolean isEntregado(); //public boolean isEntregado(){	return delivered;}
+	public boolean isEntregado(); 
 	
-	public int compareTo(Object a); 
-	/**
-
-		implementacion metodo class videojuego
-		public int compareTo(Object v) {
-			Videojuego game = (Videojuego)v;
-			int masHoras;
-	
-			if (this.horasEstimadas > game.getHorasEstimadas()) {
-				masHoras = -1;
-			}else if (this.horasEstimadas < game.getHorasEstimadas()) {
-				masHoras = 1;
-			}else {
-				masHoras = 0;
-			}
-	
-			return masHoras;
-		}
-	 *  
-	 *  
-	 *  
-	 */
-	
-	
-	
-	
+	public int compareTo(Object a); 	
 	
 }
