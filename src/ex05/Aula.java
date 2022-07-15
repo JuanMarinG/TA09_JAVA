878f0a6b2facd701a2af8219b703b63065e574a4
Arini11 <elarnaumc@gmail.com>
2022-07-15 12:48:44 +0200

    clase aula

---
+++ src/ex05/Aula.java
@@ -0,0 +1,57 @@
+package ex05;
+
+public class Aula {
+	int id;
+	int nEstudiantes;
+	Materia materia;
+	Estudiante estudiantes[];
+	Profesor profesor;
+	
+	public Aula(int id, int nEstudiantes, Materia materia, Profesor profesor, Estudiante estudiantes[]) {
+		this.id = id;
+		this.nEstudiantes = nEstudiantes;
+		this.materia = materia;
+		this.profesor = profesor;
+		this.estudiantes = estudiantes;
+	}
+	
+	public void puedeDarseClase() {
+		if(profesor.isPresente() == false) {
+			System.out.println("No se puede dar clase. El profesor no ha asistido.");
+			return;
+		}
+		if(materia != profesor.getMateria()) {
+			System.out.println("No se puede dar clase. El profesor no imparte esta materia.");
+			return;
+		}
+		if(getNumEstudiantes() <= nEstudiantes/2) {
+			System.out.println("No se puede dar clase. Se necesitan más de la mitad de alumnos.");
+			return;
+		}
+
+		System.out.println("--- HOMBRES APROBADOS ---");
+		for(Estudiante e : estudiantes) {
+			if (e.getCalificación() >= 5 && e.getSexo() == Sexo.HOMBRE)
+				System.out.println(e.toString());
+		}
+		
+		System.out.println("--- MUJERES APROBADAS ---");
+		for(Estudiante e : estudiantes) {
+			if (e.getCalificación() >= 5 && e.getSexo() == Sexo.MUJER)
+				System.out.println(e.toString());
+		}
+	}
+	
+	private int getNumAlumnos() {
+		int n = 0;
+
+		for(Estudiante e : estudiantes) {
+			if(e.getAsistencia())
+				n++;
+		}
+		return n;
+	}
+	
+	
+
+}
