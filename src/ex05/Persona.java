58071b6a68350601d623afe7b13a84027bfd0045
Joana <joanajanefdez@gmail.com>
2022-07-15 12:44:17 +0200

    abstract class Persona completada

--- src/ex05/Persona.java
+++ src/ex05/Persona.java
@@ -2,4 +2,46 @@ package ex05;
 
 public abstract class Persona {
 
+	protected String nombre;
+	protected int edad;
+	protected String genero;	
+	
+	public Persona() {
+		this.nombre = "";
+		this.edad = 0;
+		this.genero = "";
+	}
+	
+	public Persona(String nombre,int edad,String genero) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.genero = genero;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	public abstract void faltaAssistencia();
+	
 }
