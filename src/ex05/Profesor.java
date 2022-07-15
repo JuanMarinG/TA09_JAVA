eac5219174a73b27a94a878980e022b2234864d7
Joana <99611541+JoneJoana@users.noreply.github.com>
2022-07-15 12:50:49 +0200

    Update Profesor.java

--- src/ex05/Profesor.java
+++ src/ex05/Profesor.java
@@ -1,6 +1,5 @@
 package ex05;
 
-<<<<<<< HEAD
 public class Profesor extends Persona{
 	
 	private Materia materia;
@@ -21,9 +20,4 @@ public class Profesor extends Persona{
 		
 	}
 	
-	
-=======
-public class Profesor {
->>>>>>> 5e2e89e3de8a8ab85a6f4ddb53a42dc8e7d4daa8
-
 }
