5808e31a218a9fd1f9a79259361c41e2be8411d5
Arini11 <elarnaumc@gmail.com>
2022-07-16 22:12:48 +0200

    archivo reindentado

--- src/ex06/Cine.java
+++ src/ex06/Cine.java
@@ -8,29 +8,28 @@ public class Cine {
 
 	private Pelicula pelicula;
 	private double precio;
-	
+
 	private static final int FILAS = 8;
 	private static final int COLUMNAS = 9;
-	private Hashtable<String,Boolean> asientos = new Hashtable<String,Boolean>();
-	
+	private Hashtable<String, Boolean> asientos = new Hashtable<String, Boolean>();
+
 	public Cine() {
-		this(null,0);
+		this(null, 0);
 	}
-	
-	public Cine(Pelicula peli,double precio) {
+
+	public Cine(Pelicula peli, double precio) {
 		this.pelicula = peli;
 		this.precio = precio;
 		generarAsientos();
 	}
-	
-	
+
 	private void generarAsientos() {
 		// la key de cada registro del hashtable será la concatenación de fila y columna
 		// el valor será true por defecto, para indicar que el sitio está disponible
 		// accederemos a una posición así: cine.getAsientos().get("1A");
-		for (int i=1;i<=FILAS;i++) {
-			for (int j=0;j<COLUMNAS;j++) {
-				asientos.put(""+i+(char)(65+j), true);
+		for (int i = 1; i <= FILAS; i++) {
+			for (int j = 0; j < COLUMNAS; j++) {
+				asientos.put("" + i + (char) (65 + j), true);
 			}
 		}
 	}
@@ -58,32 +57,33 @@ public class Cine {
 	public void setAsientos(Hashtable<String, Boolean> asientos) {
 		this.asientos = asientos;
 	}
-	
+
 	public void sentarEspectadores(ArrayList<Espectador> espectadores) {
-        for(Espectador e : espectadores){
-            // Comprovar edad minima y dinero
-            if(e.getEdad() >= pelicula.getEdadMin() && e.getDinero() >= precio) {
-            	// Generar asiento aleatorio. Si esta ocupado, genera otro hasta que encuentre uno libre
-	            String asiento;
-	            do {
-		            asiento = "" + (int) ((Math.random() * (8 - 1)) + 1) + (char)(65+((int) ((Math.random() * (9 - 1)) + 1)));
-	            }while(!asientos.get(asiento));
-	            
-	            // Cambiar el estado del asiento a ocupado
-	            asientos.put(asiento, false);
-            }
-        }
-	}
-	
-	public void imprimirAsientosOcupados() {
-		Enumeration<String> e = asientos.keys();
-		 
-        while (e.hasMoreElements()) {
-            String key = e.nextElement();
-            if(!asientos.get(key))
-            	System.out.println(key);
-        }
+		for (Espectador e : espectadores) {
+			// Comprovar edad minima y dinero
+			if (e.getEdad() >= pelicula.getEdadMin() && e.getDinero() >= precio) {
+				// Generar asiento aleatorio. Si esta ocupado, genera otro hasta que encuentre
+				// uno libre
+				String asiento;
+				do {
+					asiento = "" + (int) ((Math.random() * (8 - 1)) + 1)
+							+ (char) (65 + ((int) ((Math.random() * (9 - 1)) + 1)));
+				} while (!asientos.get(asiento));
+
+				// Cambiar el estado del asiento a ocupado
+				asientos.put(asiento, false);
+			}
+		}
 	}
 
-	
+	public void imprimirAsientosOcupados() {
+		Enumeration<String> e = asientos.keys();
+
+		while (e.hasMoreElements()) {
+			String key = e.nextElement();
+			if (!asientos.get(key))
+				System.out.println(key);
+		}
+	}
+
 }
